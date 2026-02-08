b29fc419061b7470335da27f233329ad3a3a60f7
Guntis Ozols <guntiso@uniso.lv>
2026-02-08 15:25:18 +0200

    naming - Abc renamed to Example

--- src/main/g8/src/main/scala/Example.scala
+++ src/main/g8/src/main/scala/Example.scala
@@ -1,9 +1,9 @@
 import org.tresql._
 import org.wabase.WabaseRequestContext
 
-object Abc {
-  def fn(ctx: WabaseRequestContext ) = {
+object Example {
+  def fn(ctx: WabaseRequestContext) = {
     import ctx.wabase.tresqlResources
     tresql"""{demo(now())}"""
   }
- }
+}
