d7f7a9da821874b2049001fc591350566e7e8e0b
Guntis Ozols <guntiso@uniso.lv>
2026-02-08 19:52:16 +0200

    sbt test test

---
+++ src/main/g8/src/test/scala/ExampleSpec.scala
@@ -0,0 +1,8 @@
+import org.scalatest.flatspec.{AnyFlatSpec => FlatSpec}
+import org.scalatest.matchers.should.Matchers
+
+class ExampleSpec extends FlatSpec with Matchers {
+  it should "create db statements" in {
+    Example.createHsqldbObjectsStatements.nonEmpty shouldBe true
+  }
+}
