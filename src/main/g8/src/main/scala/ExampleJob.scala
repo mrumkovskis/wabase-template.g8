2f0ceae91ce6d5afbfdfa2e946109da098e1c85a
Guntis Ozols <guntiso@uniso.lv>
2026-02-14 20:03:11 +0200

    job example improvement - inject execution context

--- src/main/g8/src/main/scala/ExampleJob.scala
+++ src/main/g8/src/main/scala/ExampleJob.scala
@@ -1,16 +1,17 @@
 package $package_name$
 
 import java.util.concurrent.atomic.AtomicReference
+import scala.concurrent.{ExecutionContext, Future}
 
 object ExampleJob {
   private val jobStatus = new AtomicReference[String]("Job not called")
   private val Did   = "Job did something"
   private val UnDid = "Job un-did something"
 
-  def doJob = jobStatus.get match {
+  def doJob(implicit ec: ExecutionContext) = Future(jobStatus.get match {
     case Did => jobStatus.set(UnDid)
     case _   => jobStatus.set(Did)
-  }
+  })
 
   def status: String = jobStatus.get
 }
