ed9695e9d6fa7375ec7a468a43af91b8fd722859
Martins Rumkovskis <martins@uniso.lv>
2026-04-25 13:14:25 +0300

    macros fix, invocation test

--- src/main/g8/src/main/scala/macros/Macros.scala
+++ src/main/g8/src/main/scala/macros/Macros.scala
@@ -2,7 +2,7 @@ package $package_name$.macros
 
 import org.tresql.{Expr, QueryBuilder}
 
-object Macros extends org.wabase.Macros {
+class Macros extends org.wabase.Macros {
 
   def demo(b: QueryBuilder, expr: Expr): Expr = expr
 
