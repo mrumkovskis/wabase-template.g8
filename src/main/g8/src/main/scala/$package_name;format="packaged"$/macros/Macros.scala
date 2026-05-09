aaa4090053e4de75e2241d0d510c8ed8901b7b8a
Martins Rumkovskis <martins@uniso.lv>
2026-05-09 03:27:18 +0300

    resource loading refactoring, parser macro in compilation

--- src/main/g8/src/main/scala/$package_name;format="packaged"$/macros/Macros.scala
+++ src/main/g8/src/main/scala/$package_name;format="packaged"$/macros/Macros.scala
@@ -1,9 +1,14 @@
 package $package_name$.macros
 
 import org.tresql.{Expr, QueryBuilder}
+import org.tresql.parsing.QueryParsers
+import org.tresql.ast.{Exp, StringConst, Ident}
 
 object Macros extends org.wabase.Macros {
 
   def demo(b: QueryBuilder, expr: Expr): Expr = expr
-
+  // beware of parser macro written in scala, because for view compilation it would be compiled with project scala version but views are compiled with scala 2.12
+  // in this case you should configure macro sources in project/build.sbt in order to compile them with scala 2.12,
+  // MojozMacroCompile config in build.sbt should be removed.
+  //def dynamic_table(p: QueryParsers, exp: StringConst): Exp = Ident(exp.value.split("\\\\.").toList)
 }
